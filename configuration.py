195d401dbf8c9c55cbe8076e453e5927a44ec71e
Vicente Salinas <vicentesalinas@Vicentes-Laptop.attlocal.net>
2024-08-03 18:10:43 -0500

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-4df57506-7f78-4d6e-a032-45ceec69195c.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
